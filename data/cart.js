8a4ecd50324061929e4332193d5a2c6aa8d6f470
mehrab-shakib <fatscorpieeth@gmail.com>
2024-01-22 01:44:25 +0600

    using Local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,17 @@
-export let cart = [
-  { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 },
+export let cart = JSON.parse (localStorage.getItem('cart')); 
 
-  { productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 1 },
-];
+if (!cart){
+  cart = [
+    { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 },
+  
+    { productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 1 },
+  ];
+}
+
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -21,14 +30,16 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
-export function removeFromCart (productId){
-  const newCart = []; 
-  cart.forEach((cartItem)=>{
-    if (cartItem.productId !== productId){
-      newCart.push(cartItem); 
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
     }
   });
-  cart = newCart
+  cart = newCart;
+  saveToStorage();
 }
